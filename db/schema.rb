a2b77c71fc96726cf0c57a47eda96aec4ff65045
Joe Sak <joe@joesak.com>
2012-10-01 16:29:37 -0500

    Migrate the db, create the countries users join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121001211501) do
+ActiveRecord::Schema.define(:version => 20121001212348) do
+
+  create_table "countries_users", :force => true do |t|
+    t.integer  "country_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "countries_users", ["country_id"], :name => "index_countries_users_on_country_id"
+  add_index "countries_users", ["user_id"], :name => "index_countries_users_on_user_id"
 
   create_table "spree_activators", :force => true do |t|
     t.string   "description"
